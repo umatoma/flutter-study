7dc6dd25ad76d764cd3438976108927dcaf4ce5a
umatoma <umatomakun@gmail.com>
2020-05-05 23:24:13 +0900

    アフィリエイト追加

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+import styled from '@emotion/styled';
+
+const Flex = styled.div(`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+`);
+
+const FlexInner = styled.div(`
+    margin: 8px 16px;
+`);
 
 export default function Footer({ }) {
     const { site } = useStaticQuery(
@@ -21,14 +32,40 @@ export default function Footer({ }) {
     const twitterUrl = `https://twitter.com/${siteAuthor.replace('@', '')}`;
 
     return (
-        <footer id="layout-footer">
+        <div id="layout-footer">
             <p>
-                <div><a href={twitterUrl} target="_blank">{siteAuthor}</a></div>
+                <div>Flutter関連書籍</div>
+                <Flex>
+                    <FlexInner>
+                        <a href="https://www.amazon.co.jp/dp/B082XQ81FH/ref=nosim?tag=tmk555-22" target="_blank">
+                            <img src="https://images-na.ssl-images-amazon.com/images/P/B082XQ81FH.09.MZZZZZZZ.jpg" />
+                        </a>
+                    </FlexInner>
+                    <FlexInner>
+                        <a href="https://www.amazon.co.jp/dp/B07Z9DYYBY/ref=nosim?tag=tmk555-22" target="_blank">
+                            <img src="https://images-na.ssl-images-amazon.com/images/P/B07Z9DYYBY.09.MZZZZZZZ.jpg" />
+                        </a>
+                    </FlexInner>
+                    <FlexInner>
+                        <a href="https://www.amazon.co.jp/dp/B07Q19LVW2/ref=nosim?tag=tmk555-22" target="_blank">
+                            <img src="https://images-na.ssl-images-amazon.com/images/P/B07Q19LVW2.09.MZZZZZZZ.jpg" />
+                        </a>
+                    </FlexInner>
+                    <FlexInner>
+                        <a href="https://www.amazon.co.jp/dp/B07LBPRHQD/ref=nosim?tag=tmk555-22" target="_blank">
+                            <img src="https://images-na.ssl-images-amazon.com/images/P/B07LBPRHQD.09.MZZZZZZZ.jpg" />
+                        </a>
+                    </FlexInner>
+                </Flex>
             </p>
             <p>
+                <div>Twitter</div>
+                <div><a href={twitterUrl} target="_blank">{siteAuthor}</a></div>
+            </p>
+            <p className="site-title">
                 <div>{siteTitle}</div>
                 <div>© {siteAuthor.replace('@', '')}.</div>
             </p>
-        </footer>
+        </div>
     );
 };
